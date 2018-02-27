5edaf00f73609891438751518ea78f00a9d86b06
Noah Thompson <noahthompson@noah17.student.iastate.edu>
2018-02-27 14:09:54 -0600

    Testing the jawn

---
+++ Team group 6 186/Team group 6 186/itsAClass.swift
@@ -0,0 +1,35 @@
+//
+//  itsAClass.swift
+//  Team group 6 186
+//
+//  Created by Noah Thompson on 2/27/18.
+//  Copyright © 2018 Taylor Weil. All rights reserved.
+//
+
+import UIKit
+
+class itsAClass: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
