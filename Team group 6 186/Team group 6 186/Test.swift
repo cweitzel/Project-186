cd7260ace545f9251ff88b3d263179dc340f3d13
Taylor Weil <taylorcweil@yahoo.com>
2018-02-27 13:56:53 -0600

    Just some testin'

---
+++ Team group 6 186/Team group 6 186/Test.swift
@@ -0,0 +1,20 @@
+//
+//  Test.swift
+//  Team group 6 186
+//
+//  Created by Taylor Weil on 2/27/18.
+//  Copyright © 2018 Taylor Weil. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Test  {
+    
+    func testHello(test: String) -> Int {
+        if (test == "Hello"){
+            return 1;
+        }
+        return 0;
+    }
+}
