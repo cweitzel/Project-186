89bfed51214f0c8d0cabd5137c46ff21ebe43088
Noah Thompson <noahthompson@noahs-macbook-professional.local>
2018-03-01 17:55:50 -0600

    Updated GUI

--- Team group 6 186/Team group 6 186/ViewController.swift
+++ Team group 6 186/Team group 6 186/ViewController.swift
@@ -19,6 +19,19 @@ class ViewController: UIViewController {
 		button.layer.borderWidth = 1
 		textFieldName.layer.borderWidth = 1
 		textView.layer.borderWidth = 1
+		button.layer.cornerRadius = 10
+		button.clipsToBounds = true
+		textView.layer.cornerRadius=10
+		textView.clipsToBounds = true
+		textFieldName.layer.cornerRadius = 10
+		textFieldName.clipsToBounds = true
+		textView.isUserInteractionEnabled = false
+		
+		let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+		backgroundImage.image = UIImage(named: "tinge_1.jpg")
+		backgroundImage.contentMode = UIViewContentMode.scaleAspectFill
+		self.view.insertSubview(backgroundImage, at: 0)
+		
         // Do any additional setup after loading the view, typically from a nib.
     }
     override func didReceiveMemoryWarning() {
