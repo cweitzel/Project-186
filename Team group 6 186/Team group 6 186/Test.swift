bd199d0edbc0c9b695699c4eda153875378ce8ce
Taylor Weil <taylorcweil@yahoo.com>
2018-03-01 22:27:33 -0600

    good

--- Team group 6 186/Team group 6 186/Test.swift
+++ Team group 6 186/Team group 6 186/Test.swift
@@ -22,25 +22,26 @@ class Test  {
     }
     //Returns text output
     func getOut() -> String {
-		return output;
-	}
-	
-	//General Responces (Priotity 1)
-	func first(){
-		askQwershtin()
-		testNo()
-		testYes()
-		testHello()
-		testWhy()
-		testHello()
-	}
-	
-	//Additional Responcesv (Prioity 2)
-	func second(){
-		testNaughty()
-		testJawn()
-	}
-	
+        return output;
+    }
+    
+    //General Responces (Priotity 1)
+    func first(){
+        askQwershtin()
+        testNo()
+        testYes()
+        testHello()
+        testWhy()
+        testHello()
+		testYou()
+    }
+    
+    //Additional Responcesv (Prioity 2)
+    func second(){
+        testNaughty()
+        testJawn()
+    }
+    
     //Methurds
     func testHello() {
         if (input.range(of: "hello") != nil){
@@ -66,48 +67,48 @@ class Test  {
             output += ", jawn"
         }
     }
-	func testWhy() {
-		if (input.range(of: "why") != nil){
-			output = "why not?"
-		}
-	}
-	func testNo(){
-		if(input == "no"){
-			output = "Too bad :("
-		}
-	}
-	func testYes(){
-		if(input == "yes"){
-			output = "ThAt SouNdS YuMmy"
-		}
-	}
-	func askQwershtin(){
-		//Pulls file and puts everthing into an array
-		if(output == "DefaultOut"){
-			var qwersh = [String]()
-			do{
-				if let path = Bundle.main.path(forResource: "Qwershtins", ofType: "txt"){
-					let data = try String(contentsOfFile:path, encoding: String.Encoding.utf8)
-					qwersh = data.components(separatedBy: "\n")
-				}
-			}catch let err as NSError {
-				print(err)
-			}
-			//Sums up probability
-			var sum = 0
-			var pick = 0
-			for i in 0...(qwersh.count/2-1) {
-				sum = sum + Int(qwersh[(i*2)])!
-			}
-			//Picks a random line
-			var num = (Int(arc4random_uniform(UInt32(sum))))
-			while(num >= 0){
-				num = num - Int(qwersh[pick])!
-				pick += 2
-			}
-			output = qwersh[pick-1]
-		}
-	}
+    func testWhy() {
+        if (input.range(of: "why") != nil){
+            output = "why not?"
+        }
+    }
+    func testNo(){
+        if(input == "no"){
+            output = "Too bad :("
+        }
+    }
+    func testYes(){
+        if(input == "yes"){
+            output = "ThAt SouNdS YuMmy"
+        }
+    }
+    func askQwershtin(){
+        //Pulls file and puts everthing into an array
+        if(output == "DefaultOut"){
+            var qwersh = [String]()
+            do{
+                if let path = Bundle.main.path(forResource: "Qwershtins", ofType: "txt"){
+                    let data = try String(contentsOfFile:path, encoding: String.Encoding.utf8)
+                    qwersh = data.components(separatedBy: "\n")
+                }
+            }catch let err as NSError {
+                print(err)
+            }
+            //Sums up probability
+            var sum = 0
+            var pick = 0
+            for i in 0...(qwersh.count/2-1) {
+                sum = sum + Int(qwersh[(i*2)])!
+            }
+            //Picks a random line
+            var num = (Int(arc4random_uniform(UInt32(sum))))
+            while(num >= 0){
+                num = num - Int(qwersh[pick])!
+                pick += 2
+            }
+            output = qwersh[pick-1]
+        }
+    }
 	func testYou(){
 		if (input.range(of: "you?") != nil){
 			output = "Don't worry about it"
