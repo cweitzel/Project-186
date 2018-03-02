14548876b28957da73622bdf6a5aed37e92a8ee2
Taylor Weil <taylorcweil@yahoo.com>
2018-03-01 20:26:48 -0600

    new!

--- Team group 6 186/Team group 6 186/ViewController.swift
+++ Team group 6 186/Team group 6 186/ViewController.swift
@@ -11,14 +11,27 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var textFieldName: UITextField!
     @IBOutlet weak var textView: UITextView!
-    @IBOutlet weak var button: UIButton!
-    
+	@IBOutlet weak var button: UIButton!
+	
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        button.layer.borderWidth = 1
-        textFieldName.layer.borderWidth = 1
-        textView.layer.borderWidth = 1
+		button.layer.borderWidth = 1
+		textFieldName.layer.borderWidth = 1
+		textView.layer.borderWidth = 1.5
+		button.layer.cornerRadius = 10
+		button.clipsToBounds = true
+		textView.layer.cornerRadius=10
+		textView.clipsToBounds = true
+		textFieldName.layer.cornerRadius = 10
+		textFieldName.clipsToBounds = true
+		textView.isUserInteractionEnabled = false
+		
+		let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+		backgroundImage.image = UIImage(named: "tinge_1.jpg")
+		backgroundImage.contentMode = UIViewContentMode.scaleAspectFill
+		self.view.insertSubview(backgroundImage, at: 0)
+		
         // Do any additional setup after loading the view, typically from a nib.
     }
     override func didReceiveMemoryWarning() {
