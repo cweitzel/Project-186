da2e23dce257490d4d61d8cda355e05b34e2f10d
Noah Thompson <noahthompson@noah17.student.iastate.edu>
2018-04-03 13:56:49 -0500

    Added second screen

---
+++ Team group 6 186/Team group 6 186/weatherViewController.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherViewController.swift
+//  Team group 6 186
+//
+//  Created by Noah Thompson on 4/3/18.
+//  Copyright © 2018 Taylor Weil. All rights reserved.
+//
+
+import Foundation
